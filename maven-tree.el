931485ec9bc951e6de1d8c5ddcca8e4c6b90c608
Paavo <pparkkin@sandaali.(none)>
2012-07-09 19:31:23 +0300

    Create repository for Maven tree mode for Emacs.

---
+++ maven-tree.el
@@ -0,0 +1,51 @@
+
+(provide 'maven-tree)
+(eval-when-compile
+  (require 'cl))
+
+(require 'tree-mode)
+
+(define-derived-mode maven-tree-mode tree-mode "Maven tree"
+  "A mode to display a tree view of source packages in a Maven project"
+  (kill-all-local-variables)
+  (setq mode-name "Maven tree mode")
+  (setq major-mode 'maven-tree-mode)
+
+  (maven-forest
+   (list '("Project" ("main/scala" "test/scala")))))
+
+(defun maven-forest (forest)
+  (set (make-local-variable 'maven-tree)
+       (tree-mode-insert
+	(maven-tree-widgets forest))))
+
+(defun maven-tree-widgets (forest)
+  (mapcar 'maven-tree-widget forest))
+
+(defun maven-tree-widget (tree)
+  `(tree-widget
+    :node (push-button
+	   :tag ,(car tree)
+	   :format "%[%t%]\n"
+	   :notify tree-mode-reflesh-parent)
+    :expander maven-tree-widgets
+    :open nil))
+
+;; (defun maven-project-widgets (projects)
+;;   (mapcar (lambda (p)
+;; 	    (maven-tree-widget p 'maven-source-widgets))
+;; 	  projects))
+
+;; (defun maven-source-widgets (widg)
+;;   (mapcar (lambda (s)
+;; 	    (maven-tree-widget s 'maven-package-widgets))
+;; 	  (maven-sources (:tag (:node widg)))))
+
+;; (defun maven-package-widgets (widg)
+;;   nil)
+
+;; (defun maven-projects ()
+;;   (list "PRoject 1" "PRoject 2"))
+
+;; (defun maven-sources (project)
+;;   (list "main/java" "test/java"))
