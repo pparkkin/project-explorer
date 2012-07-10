8770271a8f441a4ee70ae37f93c96f1cca44a88b
Paavo <pparkkin@sandaali.(none)>
2012-07-10 08:24:14 +0300

    Fix expander.

--- maven-tree.el
+++ maven-tree.el
@@ -12,7 +12,7 @@
   (setq major-mode 'maven-tree-mode)
 
   (maven-forest
-   (list '("Project" ("main/scala" "test/scala")))))
+   (list '("Project" (("main/scala") ("test/scala"))))))
 
 (defun maven-forest (forest)
   (set (make-local-variable 'maven-tree)
@@ -20,16 +20,21 @@
 	       (maven-tree-widgets forest))))
 
 (defun maven-tree-widgets (forest)
+;  (debug)
   (mapcar 'maven-tree-widget forest))
 
 (defun maven-tree-widget (tree)
+;  (print tree) ; debug
+;  (debug)
   `(tree-widget
     :node (push-button
 	   :tag ,(car tree)
 	   :format "%[%t%]\n"
 	   :notify tree-mode-reflesh-parent)
-    :expander maven-tree-widgets
-    :open nil))
+    :expander ,(lambda (w)
+		 (maven-tree-widgets (widget-get w :child-elements)))
+    :open nil
+    :child-elements ,(car (cdr tree))))
 
 ;; (defun maven-project-widgets (projects)
 ;;   (mapcar (lambda (p)
